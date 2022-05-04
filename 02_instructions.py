ce373641c327af950500018c9c119807e7635d3c
sincla.abel <Sincla.abel@awatapu.school.nz>
2022-05-04 12:45:05 +1200

    Added instructions

---
+++ 02_instructions.py
@@ -0,0 +1,37 @@
+
+
+# functions go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        # Ask the user if they have played before
+        response = input(question).lower()
+
+        # if yes , output 'program continues'
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        # if no , output 'display instructions'
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("Please answer yes/no")
+
+
+def instructions():
+    print("**** How to play ****")
+    print()
+    print("The rules of the game go here")
+    print()
+    return ""
+
+# Main routine goes here
+played_before = yes_no("Have you played the game before")
+
+
+if played_before == "no":
+    instructions()
+print("Program Continues")
