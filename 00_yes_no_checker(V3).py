3c3feefa7ad4efb2c23c6d4be65c9c39d9537549
sincla.abel <Sincla.abel@awatapu.school.nz>
2022-05-04 12:45:42 +1200

    Added instructions

--- 00_yes_no_checker(V3).py
+++ 00_yes_no_checker(V3).py
@@ -1,23 +1,23 @@
-
-
 # functions go here...
 def yes_no(question):
     valid = False
     while not valid:
-        responce = input("Have you played this game before").lower()
+        # Ask the user if they have played before
+        response = input(question).lower()
 
-        if responce == "yes" or responce == "y":
-            responce = "yes"
-            return responce
+        # if yes , output 'program continues'
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
 
-
-        elif responce == "no" or  responce == "n":
-            responce = "no"
-            return responce
+        # if no , output 'display instructions'
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
 
         else:
             print("Please answer yes/no")
 
-# Main routine goes here...
+# Main routine goes here
 show_instructions = yes_no("Have you played the game before")
-
+print("You chose {}".format(show_instructions))
