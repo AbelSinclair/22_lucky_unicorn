ebc9758bf976d8a70b8cb26347f105f99e9ea3df
sincla.abel <Sincla.abel@awatapu.school.nz>
2022-05-17 11:37:17 +1200

    removed error

--- 05_token_genrator_v1.py
+++ 05_token_genrator_v1.py
@@ -5,6 +5,6 @@ import random
 tokens = ["Unicorn", "Horse", "Zebra", "Donkey"]
 
 # Testing loop to generate 20 tokens
-for item in range(0,20):
+for item in range(0, 20):
     chosen = random.choice(tokens)
     print(chosen, end='\t')
